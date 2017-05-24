2442412cb277711620260db2dd336699a5530576
Hal Canary <halcanary@google.com>
2017-05-24 12:36:32 -0400

    Add README

--- imgdiff.cpp
+++ imgdiff.cpp
@@ -3,39 +3,6 @@
 
   Use of this source code is governed by a BSD-style license that can be
   found in the LICENSE file.
-
-  == Image Diff ==
-
-  Provides a metric for measuring the difference between two PNG images.
-  Prints out a single floating-point number between 0.0 and 1.0.
-    0.0 means that the images are identical.
-    1.0 means that each pixel is maximally different in each channel.
-  A non-zero return value indicates that something went wrong.
-
-  This program is intended for use in scripts to sort pairs of images by
-  sameness.
-
-  On MacOS with Homebrew:
-    brew install libpng
-    git clone https://github.com/HalCanary/imagediff.git
-    cd imagediff
-    ./maker.sh "$(brew --prefix)"
-    ./imgdiff FILE1.png FILE2.png
-
-  On Posix system without libpng1.6+ already installed:
-    # Download source to libpng version 1.6+...
-    md5sum ~/Downloads/libpng-1.6.29.tar.xz
-    # verify checksum
-    cd /tmp
-    tar xf ~/Downloads/libpng-1.6.29.tar.xz
-    cd libpng-1.6.29
-    PREFIX=~/local
-    ./configure --prefix="$PREFIX" && make && make install
-    cd /tmp
-    git clone https://github.com/HalCanary/imagediff.git
-    cd imagediff
-    ./maker.sh "$PREFIX"
-    ./imgdiff FILE1.png FILE2.png
 */
 
 #include <cassert>
