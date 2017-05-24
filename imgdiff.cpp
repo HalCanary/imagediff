ccee67f46deccbad50a159120098a6eeb96f6c61
Hal Canary <halcanary@google.com>
2017-05-24 10:15:44 -0400

    Better comments and error messages.

--- imgdiff.cpp
+++ imgdiff.cpp
@@ -1,24 +1,27 @@
 /*
- * Copyright 2017 Google Inc.
- *
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
+  Copyright 2017 Google Inc.
 
-// Provides a metric for measureing the difference between two images.
-// Prints out a single floating-point number between 0.0 and 1.0.
-// 0.0 means that the images are identical.
-// 1.0 means that each pixel is maximally different in each channel.
-// A non-zero return value indicates that something went wrong.
-//
-// This program is intentionally non-verbose and non-helpful, as it is intended
-// for use in scripts to sort pairs of images by sameness.
+  Use of this source code is governed by a BSD-style license that can be
+  found in the LICENSE file.
 
-/*
+  == Image Diff ==
+
+  Provides a metric for measuring the difference between two PNG images.
+  Prints out a single floating-point number between 0.0 and 1.0.
+    0.0 means that the images are identical.
+    1.0 means that each pixel is maximally different in each channel.
+  A non-zero return value indicates that something went wrong.
+
+  This program is intended for use in scripts to sort pairs of images by
+  sameness.
+
   On MacOS:
+    git clone https://github.com/HalCanary/imagediff.git
+    cd imagediff
     brew install libpng
-    BP="$(brep --prefix)"
+    BP="$(brew --prefix)"
     CPPFLAGS="-I \"$BP/include\"" LDFLAGS="-L \"$BP/lib\"" make
+    ./imgdiff FILE1.png FILE2.png
 */
 
 #include <cassert>
@@ -42,7 +45,7 @@ Img read_png(const char* path) {
     memset(&image, 0, sizeof(image));
     image.version = PNG_IMAGE_VERSION;
     if (0 == png_image_begin_read_from_file(&image, path)) {
-        fprintf(stderr, "png error: %s\n", image.message);
+        fprintf(stderr, "png error [%s]: %s\n", path, image.message);
         exit(3);
     }
     image.format = PNG_FORMAT_RGBA;
@@ -55,7 +58,7 @@ Img read_png(const char* path) {
     output.height = image.height;
     if (0 == png_image_finish_read(&image, nullptr, output.data.get(), 0, nullptr)) {
         png_image_free(&image);
-        fprintf(stderr, "png error: %s\n", image.message);
+        fprintf(stderr, "png errori [%s]: %s\n", path, image.message);
         exit(4);
     }
     return std::move(output);
@@ -63,11 +66,15 @@ Img read_png(const char* path) {
 
 int main(int argc, char** argv) {
     if (argc != 3) {
+        const char usage[] = "\nUsage:\n  %s {FILE1}.png {FILE2}.png\n\n";
+        fprintf(stderr, usage, argv[0]);
         return 1;
     }
     Img img1 = read_png(argv[1]);
     Img img2 = read_png(argv[2]);
     if (img1.width != img2.width || img1.height != img2.height) {
+        fprintf(stderr, "\nImages must be same size: (%d,%d) != (%d,%d)\n\n",
+                img1.width, img1.height, img2.width, img2.height);
         return 2;
     }
     const size_t N = 4 * (size_t)img1.width * (size_t)img1.height;
